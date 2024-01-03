6d3699973f5382df72c79b5c60aa3cd5d3ce567e
kishor.karanjeet <kishor.karanjeet@gmail.com>
2024-01-03 16:36:33 -0600

    Adding logic to show each menu total cost

--- src/main/java/com/hamrorestaurant/billing/controller/CustomerBillingController.java
+++ src/main/java/com/hamrorestaurant/billing/controller/CustomerBillingController.java
@@ -26,10 +26,10 @@ public class CustomerBillingController {
             @ApiResponse(code = 400, message = "Bad Request. Missing required parameters", response = ErrorResponse.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class),
             @ApiResponse(code = 200, message = "Array of Lines", response = CommonResponse.class)})
-    @RequestMapping(value="/getBillAmount", produces = {"application/json"}, method= RequestMethod.PUT)
-    public CommonResponse getCustomerBilling(@RequestBody List<OrderedFood> orderedFoods){
+    @RequestMapping(value = "/getBillAmount", produces = {"application/json"}, method = RequestMethod.POST)
+    public CommonResponse getCustomerBilling(@RequestBody List<OrderedFood> orderedFoods) {
 
-            return menuService.getCustomerMenuItem(orderedFoods);
+        return menuService.getCustomerMenuItem(orderedFoods);
     }
 
     @ApiOperation(
@@ -38,8 +38,8 @@ public class CustomerBillingController {
             @ApiResponse(code = 400, message = "Bad Request. Missing required parameters", response = ErrorResponse.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class),
             @ApiResponse(code = 200, message = "Array of Lines", response = CommonResponse.class)})
-    @RequestMapping(value="/getTotalPriceForEachItem", produces = {"application/json"}, method= RequestMethod.PUT)
-    public CommonResponse getCostOfEachItem(@RequestBody List<OrderedFood> orderedFoods){
+    @RequestMapping(value = "/getTotalPriceForEachItem", produces = {"application/json"}, method = RequestMethod.POST)
+    public CommonResponse getCostOfEachItem1(@RequestBody OrderedFood orderedFoods) {
 
         return menuService.getCostOfEachItem(orderedFoods);
     }
