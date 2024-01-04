51c84e3804f5d4e4811753c87d60f7ed9b1dcb6d
kishor.karanjeet <kishor.karanjeet@gmail.com>
2024-01-03 19:43:07 -0600

    updating  getCostOfEachItem in controller

--- src/main/java/com/hamrorestaurant/billing/controller/CustomerBillingController.java
+++ src/main/java/com/hamrorestaurant/billing/controller/CustomerBillingController.java
@@ -39,8 +39,20 @@ public class CustomerBillingController {
             @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class),
             @ApiResponse(code = 200, message = "Array of Lines", response = CommonResponse.class)})
     @RequestMapping(value = "/getTotalPriceForEachItem", produces = {"application/json"}, method = RequestMethod.POST)
-    public CommonResponse getCostOfEachItem1(@RequestBody OrderedFood orderedFoods) {
+    public CommonResponse getCostOfEachItem(@RequestBody OrderedFood orderedFoods) {
 
         return menuService.getCostOfEachItem(orderedFoods);
     }
+
+//    @ApiOperation(
+//            value = "Get all user information", notes = "Get All User Information", response = CommonResponse.class, tags = {"CustomerBilling",})
+//    @ApiResponses(value = {
+//            @ApiResponse(code = 400, message = "Bad Request. Missing required parameters", response = ErrorResponse.class),
+//            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class),
+//            @ApiResponse(code = 200, message = "Array of Lines", response = CommonResponse.class)})
+//    @RequestMapping(value = "/getTotalPrice", produces = {"application/json"}, method = RequestMethod.POST)
+//    public CommonResponse getCostOfEachItem1(@RequestBody List<OrderedFood> orderedFoods) {
+//
+//        return menuService.getCustomerMenuItem(orderedFoods);
+//    }
 }
